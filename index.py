e0cc7501e050761002fd7c4d621470db3e3e0bf9
Muhammed Machrouh <56129562+medmac01@users.noreply.github.com>
2023-05-02 17:58:03 +0100

    Updated indexation script

--- index.py
+++ index.py
@@ -2,12 +2,21 @@ import os
 import json
 import PyPDF2
 import textwrap
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+model = SentenceTransformer('sentence-transformers/gtr-t5-large')
+def gtr_embedding(content):
+    # response = openai.Embedding.create(input=content,engine=engine)
+    # vector = response['data'][0]['embedding']  # this is a normal list
+    vector = model.encode(content)
+    return vector
 
 
 """
 This code iterates over a list of pdfs, extracts text and divide it into chunks. Then it saves the chunks into a json file.
 """
-pdf_dir = "/Users/medmachrouh/Desktop/PFA/Dataset/Dataset/Merged/"
+pdf_dir = os.path.join(os.getcwd(), 'Dataset')
 json_file = os.getcwd()+'/index_2.json'
 
 pdf_files = [f for f in os.listdir(pdf_dir) if f.endswith(".pdf")]
@@ -24,11 +33,14 @@ for pdf_file in pdf_files:
             page = pdf_reader.pages[page_num]
             content = page.extract_text()
             chunks = textwrap.wrap(content, 1000)
+            
             for chunk in chunks:
+                embed = gtr_embedding(chunk)
                 output.append({
                     "filename": pdf_file,
                     "page_number": page_num+1,
-                    "content": chunk
+                    "content": chunk,
+                    "embedding" : np.array(embed).tolist()
                 })
 
 with open(json_file, "w") as f:
