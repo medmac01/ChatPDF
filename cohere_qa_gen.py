7d1f88c5e101b0f9c371a7d457e238f75c90ce61
Muhammed Machrouh <56129562+medmac01@users.noreply.github.com>
2023-06-21 00:38:33 +0100

    Add files via upload

--- cohere_qa_gen.py
+++ cohere_qa_gen.py
@@ -2,23 +2,22 @@ import json
 import re
 import cohere, openai
 import time
+import requests
 
-# openai.api_key = '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'
-co = cohere.Client('nn1vbXB6F6uvuQfetzMRVdttxiKQf1Ch26I0sh5Q')
 
 # Load existing JSON data from the file
-with open("index_2.json", "r") as json_file:
+with open("data.json", "r") as json_file:
     passages_data = json.load(json_file)
 
 # Initialize the list to store the generated QA pairs
-checkpoint = passages_data[1400:]
+checkpoint = passages_data[4350:]
 counter = 0
 print(len(passages_data))
 # Generate QA pairs for each passage
 for entry in checkpoint:
     try:
         generated_qa_pairs = []
-        context = entry["content"]
+        context = entry["text"]
         print(f'Trying this passage {context[:100]}')
         # response = openai.Completion.create(
         #       model="text-davinci-003",
@@ -30,30 +29,48 @@ for entry in checkpoint:
         #       presence_penalty=0
         #     )
 
-        response = co.generate(
-            model='command',
-            prompt=f'Generer cinq question (pas qcm) en relation au context du passage suivant, chaque question doit avoir sa réponse sous la forme suivante:\n\n{{ question: \"\",\nreponse: \"\"}}\n\nPassage: {context}\n\nQuestion:',
-            max_tokens=300,
-            temperature=0.7,
-            k=0,
-            stop_sequences=[],
-            return_likelihoods='NONE'
-        )
+        # response = co.generate(
+        #     model='command',
+        #     prompt=f'Generate a question related to the following passage, each question should have also its answer.The answer should be elaborated in a sentence or a short paragraph, but not a single word.\n\nPassage: {context}\n\nQuestion:',
+        #     max_tokens=300,
+        #     temperature=0.7,
+        #     k=0,
+        #     stop_sequences=[],
+        #     return_likelihoods='NONE'
+        # )
+
+
+
+        API_URL = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct"
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+    
+        output = query({
+            "inputs": f"Generate a question related to the following passage, each question should have also its answer.The answer should be elaborated in a sentence or a short paragraph, but not a single word.\n\nPassage: {context}\n\nQuestion:",
+            "parameters": {"max_new_tokens":600}
+        })
+        # print(output)
+
         print(f'Attempt {counter} before sleeping')
         counter = counter + 1
 
         # Extract the generated question and answer from the response using regex
-        generated_text = response.generations[0].text.strip()
+        # generated_text = response.generations[0].text.strip()
+        output = output[0]['generated_text']
 
         # Split the generated text into question and answer parts
-        parts = re.split(r"Réponse:|Answer:|reponse:|answer:|Reponse:|réponse:", generated_text)
-        if len(parts) == 2:
-            generated_question = parts[0].strip()
-            generated_answer = parts[1].strip()
-        else:
-            generated_question = generated_text
-            generated_answer = ""
+        # parts = re.split(r"Réponse:|Answer:|reponse:|answer:|Reponse:|réponse:|ANSWER:", generated_text)
+        # if len(parts) == 2:
+        #     generated_question = parts[0].strip()
+        #     generated_answer = parts[1].strip()
+        # else:
+        #     generated_question = generated_text
+        #     generated_answer = ""
 
+        generated_question = output[output.find("Question:"):output.find("Answer:")-1]
+        generated_answer = output[output.find("Answer:"):]
         # Create a new QA pair
         qa_pair = {
             "context": context,
